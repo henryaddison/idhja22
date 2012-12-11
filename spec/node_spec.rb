6429d8d18cfa9d757450906ed865785ac9163c77
Henry Addison <henry@dressipi.com>
2012-12-11 16:51:54 +0000

    added a spec for DecisionNode#get_rules

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -1,12 +1,5 @@
 require 'spec_helper'
 
-describe Idhja22::Node do
-  before(:all) do
-    @ds = Idhja22::Dataset.from_csv(File.join(File.dirname(__FILE__),'large_spec_data.csv'))
-  end
-
-end
-
 describe Idhja22::LeafNode do
   describe('#get_rules') do
     it 'should return the probability' do
@@ -18,7 +11,14 @@ describe Idhja22::LeafNode do
 end
 
 describe Idhja22::DecisionNode do
+  before(:all) do
+    @ds = Idhja22::Dataset.from_csv(File.join(File.dirname(__FILE__),'large_spec_data.csv'))
+  end
+
   describe('#get_rules') do
-    
+    it 'should return a list of rules' do
+      l = Idhja22::DecisionNode.new(@ds.split(2), '3', [],   0.75)
+      l.get_rules.should == ["3 == a and then chance of liking = 0.75", "3 == b and then chance of liking = 0.75"]
+    end
   end
 end
