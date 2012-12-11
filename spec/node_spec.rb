af065294a100885593abe3f667d8f501a9f52a81
Henry Addison <henry@dressipi.com>
2012-12-11 17:01:38 +0000

    removed some messy whitespace

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -7,7 +7,6 @@ describe Idhja22::LeafNode do
       l.get_rules.should == ['then chance of liking = 0.75']
     end
   end
-  
 end
 
 describe Idhja22::DecisionNode do
@@ -17,7 +16,7 @@ describe Idhja22::DecisionNode do
 
   describe('#get_rules') do
     it 'should return a list of rules' do
-      l = Idhja22::DecisionNode.new(@ds.split(2), '3', [],   0.75)
+      l = Idhja22::DecisionNode.new(@ds.split(2), '3', [], 0.75)
       l.get_rules.should == ["3 == a and then chance of liking = 0.75", "3 == b and then chance of liking = 0.75"]
     end
   end
