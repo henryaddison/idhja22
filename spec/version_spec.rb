0f8da9505d016f5a8485a50e475554a98c951014
Henry Addison <henry@dressipi.com>
2012-12-12 16:30:49 +0000

    bumped version for new entropy calculation

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Idhja22 do
   describe 'VERSION' do
     it 'should be current version' do
-      Idhja22::VERSION.should == '0.8.0'
+      Idhja22::VERSION.should == '0.9.0'
     end
   end
 end
