de8cdba5e0ae4faab5c6800b9fdbee0e5720efe7
Henry Addison <henry@dressipi.com>
2012-12-17 11:10:32 +0000

    bumped minor version for validation methods

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Idhja22 do
   describe 'VERSION' do
     it 'should be current version' do
-      Idhja22::VERSION.should == '0.12.1'
+      Idhja22::VERSION.should == '0.13.0'
     end
   end
 end
