47ceaf7e0583028f489c34bfa60dbbc252554036
Henry Addison <henry@dressipi.com>
2012-12-17 14:31:19 +0000

    added documentation to the main Tree entry class methods

--- lib/idhja22/tree.rb
+++ lib/idhja22/tree.rb
@@ -1,11 +1,16 @@
 module Idhja22
+  # The main entry class for a training, viewing and evaluating a decision tree.
   class Tree
     attr_accessor :root
     class << self
+      # Trains a Tree using the provided Dataset.
       def train(dataset)
         new(dataset, dataset.attribute_labels)
       end
 
+      # Takes a dataset and splits it randomly into training and validation data. 
+      # Uses the training data to train a tree whose perfomance then measured using the validation data.
+      # @param [Float] Proportion of dataset to use for training. The rest will be used to validate the resulting tree.
       def train_and_validate(dataset, training_proportion=0.5)
         training_set, validation_set = dataset.split(training_proportion)
         tree = self.train(training_set)
@@ -13,11 +18,15 @@ module Idhja22
         return tree, validation_value
       end
 
+      # see #train
+      # @note Takes a CSV filename rather than a Dataset
       def train_from_csv(filename)
         ds = Dataset.from_csv(filename)
         train(ds)
       end
 
+      # see #train_and_validate
+      # @note Takes a CSV filename rather than a Dataset
       def train_and_validate_from_csv(filename, training_proportion=0.5)
         ds = Dataset.from_csv(filename)
         train_and_validate(ds, training_proportion)
