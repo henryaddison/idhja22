afd3fa8511599a1733e0fdf7098037c823795032
Henry Addison <henry@dressipi.com>
2012-12-17 17:40:58 +0000

    a default skeleton config for gem

--- idhja22.gemspec
+++ idhja22.gemspec
@@ -22,4 +22,6 @@ Gem::Specification.new do |gem|
   gem.add_development_dependency 'simplecov'
   gem.add_development_dependency 'yard'
   gem.add_development_dependency 'redcarpet'
+
+  gem.add_dependency 'configuration'
 end
--- lib/idhja22.rb
+++ lib/idhja22.rb
@@ -1,11 +1,17 @@
+require 'configuration'
+require 'idhja22/config/default'
+
 require "idhja22/version"
 require "idhja22/dataset"
 require "idhja22/tree"
 require "idhja22/bayes"
 
-
 module Idhja22
   DEFAULT_PROBABILITY = 0.5
   TERMINATION_PROBABILITY = 0.95
   MIN_DATASET_SIZE = 20
+
+  def self.config
+    @config ||= Configuration.for('default')
+  end
 end
