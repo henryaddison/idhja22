543e8f7e85bf120b5b9e4c6483ecd8022c38a431
Henry Addison <henry@dressipi.com>
2012-12-18 13:30:39 +0000

    added a basic training functionality to bayes classifier

--- lib/idhja22/bayes.rb
+++ lib/idhja22/bayes.rb
@@ -1,5 +1,39 @@
 module Idhja22
   class Bayes
-    
+    attr_accessor :conditional_probabilities, :prior_probabilities
+    class << self
+      def train dataset, attribute_labels_to_use
+        classifier = new
+        classifier.conditional_probabilities = calculate_conditional_probabilities(dataset, attribute_labels_to_use)
+        classifier.prior_probabilities = calculate_priors(dataset)
+
+        return classifier
+      end
+
+      def calculate_conditional_probabilities dataset, attribute_labels_to_use
+        conditional_probabilities = {}
+        attribute_labels_to_use.each do |attr_label|
+          conditional_probabilities[attr_label] = {}
+          partitioned_data = dataset.partition(attr_label)
+          partitioned_data.each do |attr_value, ds|
+            conditional_probabilities[attr_label][attr_value] = Hash.new(0)
+            ds.partition_by_category.each do |cat, uniform_ds|
+              conditional_probabilities[attr_label][attr_value][cat] = uniform_ds.size.to_f/ds.size.to_f
+            end
+          end
+        end
+
+        return conditional_probabilities
+      end
+
+      def calculate_priors dataset
+        output = Hash.new(0)
+        dataset.category_counts.each do |cat, count|
+          output[cat] = count.to_f/dataset.size.to_f
+        end
+        return output
+      end
+    end
+
   end
 end
--- spec/bayes_spec.rb
+++ spec/bayes_spec.rb
@@ -1,5 +1,61 @@
 require 'spec_helper'
 
 describe Idhja22::Bayes do
+  before(:all) do
+    @ds = Idhja22::Dataset.from_csv(File.join(File.dirname(__FILE__),'large_spec_data.csv'))
+  end
 
+  describe '.train' do
+    it 'should train a classifier from a dataset' do
+      classifier = Idhja22::Bayes.train @ds, %w{0}
+      cond_probs = classifier.conditional_probabilities
+      cond_probs.keys.should == ['0']
+      cond_probs['0'].keys.should == ['a','b']
+      cond_probs['0']['a']['Y'].should == 0.625
+      cond_probs['0']['a']['N'].should == 0.375
+
+      cond_probs['0']['b']['Y'].should == 0.5
+      cond_probs['0']['b']['N'].should == 0.5
+
+      prior_probs = classifier.prior_probabilities
+      prior_probs['Y'].should == 0.6
+      prior_probs['N'].should == 0.4
+    end
+  end
+
+  describe '.calculate_conditional_probabilities' do
+    it 'should calculate the conditional probabilities of P(Cat|attr_val) from dataset for given attribute labels' do
+      cond_probs = Idhja22::Bayes.calculate_conditional_probabilities @ds, %w{0 2}
+      cond_probs.keys.should == ['0', '2']
+      cond_probs['0'].keys.should == ['a','b']
+      cond_probs['2'].keys.should == ['a','b']
+
+      cond_probs['0']['a']['Y'].should == 0.625
+      cond_probs['0']['a']['N'].should == 0.375
+      cond_probs['0']['b']['Y'].should == 0.5
+      cond_probs['0']['b']['N'].should == 0.5
+
+      cond_probs['2']['a']['Y'].should == 0.75
+      cond_probs['2']['a']['N'].should == 0.25
+      cond_probs['2']['b']['Y'].should == 0
+      cond_probs['2']['b']['N'].should == 1.0
+    end
+  end
+
+  describe '.calculate_priors' do
+    it 'should calculate the prior probabilities' do
+      prior_probs = Idhja22::Bayes.calculate_priors @ds
+      prior_probs['Y'].should == 0.6
+      prior_probs['N'].should == 0.4
+    end
+
+    context 'all single category' do
+      it 'should return 0 for other categories' do
+        uniform_ds = Idhja22::Dataset.new([Idhja22::Dataset::Example.new(['high', '20-30', 'vanilla', 'Y'], ['Confidence', 'Age group', 'fav ice cream'] , 'Loves Reading')], ['Confidence', 'Age group', 'fav ice cream'], 'Loves Reading')
+        prior_probs = Idhja22::Bayes.calculate_priors uniform_ds
+        prior_probs['Y'].should == 1.0
+        prior_probs['N'].should == 0
+      end
+    end
+  end
 end
