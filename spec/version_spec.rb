60a6f627e1f3a373f9804218f726a5ca3e752e92
Henry Addison <henry@dressipi.com>
2012-12-18 12:09:10 +0000

    bumped patch version as Dataset#category_count uses new Dataset#category_split method

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Idhja22 do
   describe 'VERSION' do
     it 'should be current version' do
-      Idhja22::VERSION.should == '0.14.4'
+      Idhja22::VERSION.should == '0.14.5'
     end
   end
 end
