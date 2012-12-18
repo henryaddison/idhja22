ee7a9ee96dcd05320d7c238c40c1f1ef7a0a3e86
Henry Addison <henry@dressipi.com>
2012-12-18 16:12:20 +0000

    bumped patch version for bug fix

--- lib/idhja22/version.rb
+++ lib/idhja22/version.rb
@@ -1,3 +1,3 @@
 module Idhja22
-  VERSION = "1.0.0"
+  VERSION = "1.0.1"
 end
--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Idhja22 do
   describe 'VERSION' do
     it 'should be current version' do
-      Idhja22::VERSION.should == '1.0.0'
+      Idhja22::VERSION.should == '1.0.1'
     end
   end
 end
